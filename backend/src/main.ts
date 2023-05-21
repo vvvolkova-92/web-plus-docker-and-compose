aa91dee3cb0556b840b4d23c5a9f468bd77d90ca
vvvolkova-92 <saiviolet@gmail.com>
2023-05-21 19:36:13 +0300

    FIX / CORS + GITIGNORE

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from "./app.module";
 import * as process from "process";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT);
   console.log(`Сервер запущен`);
 }
